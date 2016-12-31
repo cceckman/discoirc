e1e13eeb0caee375b254a58b779f4c1f0f608f49
Charles Eckman <charles@cceckman.com>
2016-12-30 16:48:12 -0800

    Handle termination more gracefully, in theory.

--- prototype/termui/bgproc/main.go
+++ prototype/termui/bgproc/main.go
@@ -2,22 +2,42 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"os"
+	"os/signal"
+	"syscall"
 
 	flog "github.com/cceckman/discoirc/prototype/termui/log"
 	"log"
 
 	"github.com/cceckman/primes"
 
-	"time"
 	"net"
+	"time"
 )
 
 var (
 	help = flag.Bool("help", false, "Display a usage message.")
 )
+
+// signalContext returns a Context that ends with SIGINT or SIGTERM.
+func signalContext() context.Context {
+	ctx, cancel := context.WithCancel(context.Background())
+
+	signals := make(chan os.Signal, 1)
+
+	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		sig := <-signals
+		log.Printf("Received signal %s, starting shutdown", sig.String())
+		cancel()
+	}()
+
+	return ctx
+}
 
 func main() {
 	flag.Usage = func() {
@@ -38,18 +58,35 @@ func main() {
 	flog.LogArgs()
 	// Above is boilerplate.
 
-
 	// Create a Unix domain socket.
 	network := "unix"
 	addr := "/tmp/discod"
-	l, err := net.ListenUnix(network, &net.UnixAddr{addr, network})
+	l, err := net.ListenUnix(network, &net.UnixAddr{
+		Name: addr,
+		Net:  network,
+	})
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer os.Remove(addr)
+	defer func() {
+		log.Printf("Removing %s\n", addr)
+		err := os.Remove(addr)
+		if err != nil {
+			log.Printf("couldn't remove %s: %v\n", addr, err)
+		}
+	}()
+
+	ctx := signalContext()
 
 	p := primes.NewMemoizingPrimer()
-	for cid := 0; true; cid++{
+	for cid := 0; true; cid++ {
+		select {
+		case <-ctx.Done():
+			break
+		default:
+			// pass
+		}
+
 		log.Println("Awaiting connection...")
 		newCon, err := l.AcceptUnix()
 		if err != nil {
@@ -58,7 +95,7 @@ func main() {
 		log.Printf("Got a new connection (%d)\n", cid)
 
 		// Start a background writer.
-		go func(n int, conn net.Conn) {
+		go func(ctx context.Context, n int, conn net.Conn) {
 			// TODO: needs to reliably handle closed other end.
 			defer conn.Close()
 
@@ -69,10 +106,15 @@ func main() {
 			p.PrimesUpTo(1000000, c)
 
 			for prime := range c {
-				<-ticker.C
+				select {
+				case <-ctx.Done():
+					return
+				case <-ticker.C:
+					// pass, see below.
+				}
 				fmt.Printf("Tick on %d: %d\n", n, prime)
 				fmt.Fprintf(conn, "%07d\n", prime)
 			}
-		}(cid, newCon)
+		}(ctx, cid, newCon)
 	}
 }
