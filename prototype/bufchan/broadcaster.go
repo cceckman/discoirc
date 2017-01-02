e166a614ba81b19161a8a2607c773564f8381e36
Charles Eckman <charles@cceckman.com>
2017-01-02 03:12:45 -0800

    Use channels rather than locking.
    Which should be better- it is easier to reason about, really- but
    still has two big failings:
    - leaks a thread, and
    - nondeterministic test timings -> test results.

--- prototype/bufchan/broadcaster.go
+++ prototype/bufchan/broadcaster.go
@@ -3,7 +3,6 @@ package bufchan
 
 import (
 	"context"
-	"sync"
 )
 
 // A Broadcaster listens on a string channel, and writes to all of several listeners.
@@ -14,7 +13,11 @@ type Broadcaster interface {
 
 // Creates a new Broadcaster on the given input channel.
 func NewBroadcaster(c <-chan string) Broadcaster {
-	r := &listBroadcaster{input: c}
+	r := &listBroadcaster{
+		input:        c,
+		rmReceivers:  make(chan *Bufchan),
+		addReceivers: make(chan *Bufchan),
+	}
 	go r.broadcast()
 	return r
 }
@@ -23,55 +26,73 @@ func NewBroadcaster(c <-chan string) Broadcaster {
 type listBroadcaster struct {
 	input <-chan string
 
-	receivers []*Bufchan
-	sync.RWMutex
+	receivers    []*Bufchan
+	rmReceivers  chan *Bufchan
+	addReceivers chan *Bufchan
 }
 
-// broadcast listens on input, and writes to receivers.
+// broadcast is the main channel of a listBroadcaster; it listens for input or add / remove requests,
+// and writes to its receivers.
 func (lb *listBroadcaster) broadcast() {
-	for {
-		x, ok := <-lb.input
-		lb.RLock()
-		// Don't defer; explicitly unlock after loop over receivers.
-		for _, receiver := range lb.receivers {
-			if ok {
-				receiver.In() <- x
+	closed := false
+	for !closed {
+		select {
+		case rm := <-lb.rmReceivers:
+			for i, receiver := range lb.receivers {
+				if receiver == rm {
+					// Nice pointer: https://github.com/golang/go/wiki/SliceTricks
+					// "delete without preserving order". That's fine.
+					newLen := len(lb.receivers) - 1
+					lb.receivers[i] = lb.receivers[newLen]
+					lb.receivers[newLen] = nil // Allow GC of the *Bufchan (once other threads complete too.)
+					lb.receivers = lb.receivers[:newLen]
+					break
+				}
+			}
+		case add := <-lb.addReceivers:
+			lb.receivers = append(lb.receivers, add)
+		case x, ok := <-lb.input:
+			if !ok {
+				closed = true
 			} else {
-				close(receiver.In())
+				for _, receiver := range lb.receivers {
+					receiver.In() <- x
+				}
 			}
 		}
-		lb.RUnlock()
+	}
 
-		if !ok {
-			// Input is closed, all receivers closed in above. All done!
-			break
+	// Input channel is closed. Close all current receivers.
+	for i, r := range lb.receivers {
+		close(r.In())
+		lb.receivers[i] = nil // allow GC, when other threads complete.
+	}
+
+	// Hang around indefinitely to handle the add / remove channels.
+	// TODO: I don't like this. This tail-end leaks the entire listBroadcaster, keeping it around
+	// for the lifetime of the program.
+	// I think there's a way to do it with a done-channel alongside Input,
+	// s.t. Listen can return immediately, but I"m too tired to think of it at the moment; seems like
+	// everything's coming up races.
+	for {
+		select {
+		case <-lb.rmReceivers:
+			// pass; we've already cleared lb.receivers.
+		case add := <-lb.addReceivers:
+			// Input is closed, so close the receiver's input too.
+			close(add.In())
 		}
 	}
 }
 
 func (lb *listBroadcaster) Listen(ctx context.Context) <-chan string {
-	lb.Lock()
-	defer lb.Unlock()
-
 	n := New(ctx)
-	lb.receivers = append(lb.receivers, n)
+	lb.addReceivers <- n
 	go func() {
 		// Remove from receivers once the context is cancelled.
+		// The case of "if input is closed" is handled by the main broadcast loop.
 		<-ctx.Done()
-
-		lb.Lock()
-		defer lb.Unlock()
-		for i, r := range lb.receivers {
-			if r == n {
-				// Nice pointer: https://github.com/golang/go/wiki/SliceTricks
-				// "delete without preserving order". That's fine.
-				newLen := len(lb.receivers) - 1
-				lb.receivers[i] = lb.receivers[newLen]
-				lb.receivers[newLen] = nil // Allow GC of the *Bufchan (once other threads complete too.)
-				lb.receivers = lb.receivers[:newLen]
-				break
-			}
-		}
+		lb.rmReceivers <- n
 	}()
 
 	return n.Out()
