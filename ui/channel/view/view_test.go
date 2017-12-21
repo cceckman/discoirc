9eadc826e0c59f4bc912db2c2f7a70cb756ea52d
Charles Eckman <charles@cceckman.com>
2017-12-21 13:37:41 -0800

    Add test for re-drawing

--- ui/channel/view/view_test.go
+++ ui/channel/view/view_test.go
@@ -7,7 +7,7 @@ import (
 	"testing"
 )
 
-func TestView(t *testing.T) {
+func TestView_SimpleRender(t *testing.T) {
 	surface := tui.NewTestSurface(40, 10)
 	theme := tui.NewTheme()
 	theme.SetStyle("reversed", tui.Style{Reverse: tui.DecorationOn})
@@ -21,6 +21,62 @@ func TestView(t *testing.T) {
 	v.SetMode("+v")
 	v.SetEvents(mocks.Events)
 
+	p.Repaint(v)
+
+	wantDecorations := `
+1111111111111111111111111111111111111111
+0000000000000000000000000000000000000000
+0000000000000000000000000000000000000000
+0000000000000000000000000000000000000000
+0000000000000000000000000000000000000000
+0000000000000000000000000000000000000000
+0000000000000000000000000000000000000000
+0000000000000000000000000000000000000000
+1111111111111111111111111111111111111111
+0000000000000000000000000000000000000000
+`
+	gotDecorations := surface.Decorations()
+	if gotDecorations != wantDecorations {
+		t.Errorf("unexpected decorations: got = \n%s\nwant = \n%s", gotDecorations, wantDecorations)
+	}
+
+	wantContents := `
+topic                                   
+vnfold your selfe                       
+1,6 <barnardo> Long liue the King       
+2,1 <claudius> Welcome, dear Rosencrantz
+and Guildenstern!                       
+2,2 <gertrude> Good gentlemen, he hath  
+much talk'd of you;                     
+2,3 <rosencrantz> Both your majesties   
+network: connected channel: joined +v   
+<nick>                                  
+`
+	gotContents := surface.String()
+	if gotContents != wantContents {
+		t.Errorf("unexpected contents: got = \n%s\nwant = \n%s", gotContents, wantContents)
+	}
+}
+
+func TestView_(t *testing.T) {
+	// Render with a different size.
+	preSurface := tui.NewTestSurface(80, 20)
+	theme := tui.NewTheme()
+	theme.SetStyle("reversed", tui.Style{Reverse: tui.DecorationOn})
+	prePainter := tui.NewPainter(preSurface, theme)
+
+	surface := tui.NewTestSurface(40, 10)
+	p := tui.NewPainter(surface, theme)
+
+	v := view.New(&mocks.UI{})
+	v.SetTopic("topic")
+	v.SetNick("<nick>")
+	v.SetConnection("network: connected")
+	v.SetPresence("channel: joined")
+	v.SetMode("+v")
+	v.SetEvents(mocks.Events)
+
+	prePainter.Repaint(v)
 	p.Repaint(v)
 
 	wantDecorations := `
