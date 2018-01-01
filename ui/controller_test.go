dc328dff2a172eac247444940e7e6a63400a4865
Charles Eckman <charles@cceckman.com>
2018-01-01 12:34:37 -0800

    add test for channel activation

--- ui/controller_test.go
+++ ui/controller_test.go
@@ -4,31 +4,22 @@ import (
 	"testing"
 
 	"github.com/cceckman/discoirc/ui"
-	_ "github.com/cceckman/discoirc/ui/channel"
+	"github.com/cceckman/discoirc/ui/channel"
 	"github.com/cceckman/discoirc/ui/client"
 	"github.com/cceckman/discoirc/ui/mocks"
 )
 
 func TestActivateChannel(t *testing.T) {
-	/* TODO: Disabled until implemented
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
+	u := mocks.NewUI()
 
-	u := discomocks.NewUI()
-	u.Update(func() {
-		u.SetWidget(widgets.NewSplash())
-	})
+	ctl := ui.New(u, mocks.NewBackend())
 
-	ctl := ui.New(ctx, u)
-
-	u.Add(1) // Expect one update to change root, keybindings, etc.
-	ctl.ActivateChannel("foonet", "#discoirc")
+	ctl.ActivateChannel("foonet", "#barchan")
 	u.RunSync(func() {
-		if _, ok := u.Root.(channel.View); !ok {
-			t.Errorf("unexpected view at UI root: got: %+v want: controller.View", u.Root)
+		if _, ok := u.Root.(*channel.View); !ok {
+			t.Errorf("unexpected view at UI root: got: %+v want: client.View", u.Root)
 		}
 	})
-	*/
 }
 
 func TestActivateClient(t *testing.T) {
@@ -37,7 +28,6 @@ func TestActivateClient(t *testing.T) {
 	ctl := ui.New(u, mocks.NewBackend())
 
 	ctl.ActivateClient()
-	// Panic: negative WG counter
 	u.RunSync(func() {
 		if _, ok := u.Root.(client.View); !ok {
 			t.Errorf("unexpected view at UI root: got: %+v want: client.View", u.Root)
